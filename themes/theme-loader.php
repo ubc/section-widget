42bfd63337093488f70fc7bfb27af26628f0982d
Kelvin Xu <kelvin.xu@ubc.ca>
2025-05-02 12:40:29 -0700

    Fixed Path Traversal Vulnerability

--- themes/theme-loader.php
+++ themes/theme-loader.php
@@ -3,18 +3,18 @@
 $theme = isset($_GET['theme'])? strtolower(trim($_GET['theme'])) : 'base';
 $scope = isset($_GET['scope'])? trim($_GET['scope']) : '';
 
-$content = @file_get_contents("./{$theme}/sw-theme.css");
+$content = @file_get_contents(SECTION_WIDGET_DIR_PATH . "themes/{$theme}/sw-theme.css");
 
 if(!$content) {
     // Try again with the default theme
-    $content = @file_get_contents("./base/sw-theme.css");
+    $content = @file_get_contents(SECTION_WIDGET_DIR_PATH . "/themes/base/sw-theme.css");
 }
 
 if($content) {
     $content = str_replace('%scope%', $scope, $content);
     $content = str_replace('url(images', "url({$theme}/images", $content);
     header('Content-Type: text/css');
-    echo $content;
+    echo wp_kses_post($content);
 }
 
 ?>
